71bb0b16c3f1b53496f5bdc71dcba723d8a275d7
Dhel-Phyn <dhelphyn@gmail.com>
2022-08-01 16:19:36 +0000

    Step -1

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("i am a girl");
+
+// DOM manipulation
+// add a text inside a tag
+document.getElementById("first").innerHTML = "my first dom manipulation";
+
+// change text color
+document.getElementById("first").style.color = "brown";
+document.getElementById("first").style.backgroundColor = "purple";
+document.getElementById("first").style.fontSize = "30px";
+
+// display text as none
+document.getElementById("first").style.display = "none"
